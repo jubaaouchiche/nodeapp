be6ae0f241753e3f0b2ae3a66d7b71eb5066d8a2
jubaaouchiche <63096465+jubaaouchiche@users.noreply.github.com>
2026-02-19 11:12:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Devops from MyProject V4 Groupe21!!!');
+  res.send('Hello TOTO !!!');
 });
 
 const PORT = process.env.PORT || 3000;
